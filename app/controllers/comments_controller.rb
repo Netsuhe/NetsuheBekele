e7bb2552e87f8579f1e2ef2d92682cd4297c5fd8
Netsuhe <Netsuhebekele@gmail.com>
2017-09-20 11:31:44 -0400

    Fixed img issue and Full CRUD

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,11 +8,12 @@ class CommentsController < ApplicationController
 
   def show
     @comment = Comment.find(params[:id])
+    # redirect_to new_comment_path
   end
 
   def create
-    @comment = Comment.create(comment_params)
-    redirect_to
+    @comment = Comment.create!(comment_params)
+    redirect_to comments_path
   end
 
   def edit
@@ -20,11 +21,15 @@ class CommentsController < ApplicationController
   end
 
   def update
-    @comment = C
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+    redirect_to comments_path
   end
 
   def destroy
-
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to comments_path
   end
 
   private
